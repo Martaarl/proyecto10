9e26d266fed7119990e21a6ba0596d17b0af4828
Martaarl <marali23@gmail.com>
2026-03-17 10:56:52 +0100

    renderizo post y subo más contenido

--- frontend/posts.js
+++ frontend/posts.js
@@ -1,7 +1,4 @@
 
-
-console.log("Frontend conectado");
-
 const container = document.getElementById("posts-container");
 
 const getPost = async () => {
@@ -9,13 +6,34 @@ const getPost = async () => {
         const res = await fetch("http://localhost:3000/posts");
         const posts = await res.json();
 
+        container.innerHTML = "";
+
         if (posts.length === 0) {
             container.innerHTML = "<p>No hay posts todavía</p>";
             return;
         }
-
+        
         posts.forEach(post => {
-            console.log(post);
+            const articlePost = document.createElement("article");
+            articlePost.className="Card-Post"
+
+            const titlePost = document.createElement("h2");
+            titlePost.className = "Title-Post";
+            titlePost.textContent = post.title;
+            
+            const imgPost = document.createElement("img");
+            imgPost.src = post.image.url;
+            imgPost.alt = post.title;
+            imgPost.className= "Image-Post";
+
+            const descriptionPost = document.createElement("p");
+            descriptionPost.textContent = post.description;
+            descriptionPost.className ="Description-Post";
+
+            articlePost.append(titlePost, imgPost, descriptionPost);
+
+            container.appendChild(articlePost);
+
         });
 
     } catch (error) {
